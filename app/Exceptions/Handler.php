ed8bf125f6643a223b9bdab03e5d07297d9b892a
SUBHANI SHAIK <subhanisk532@gmail.com>
2021-05-28 14:51:29 +0530

    Add files via upload

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,11 +7,6 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
     protected $dontReport = [
         //
     ];
